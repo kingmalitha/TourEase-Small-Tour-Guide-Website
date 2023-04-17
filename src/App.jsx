57ee2080891ae5e41810a21ebb12a1178ecba5b4
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-04-17 15:43:17 +0530

    03. NavBar finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import TopBar from "./components/TopBar";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
       <TopBar />
+      <NavBar />
     </>
   );
 };
